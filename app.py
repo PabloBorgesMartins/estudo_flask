eb33340064b41d172051e18194ad5f0e9f8c07f9
Pablo Borges Martins <pablo.martins1998@gmail.com>
2025-07-23 02:04:14 -0300

    feat: some new routes

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
+from flask_login import UserMixin, login_user, LoginManager
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ecommerrce.db'
+app.config["SECRET_KEY"] = "minha_key"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ecommerrce.db"
 
+login_manager = LoginManager()
 db = SQLAlchemy(app)
+login_manager.init_app(app)
+login_manager.login_view = "login"
+CORS(app)
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=True)
+
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -12,28 +26,93 @@ class Product(db.Model):
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
 
-@app.route('/api/products/add', methods=["POST"])
+
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json
+    user = User.query.filter_by(username=data.get("username")).first()
+    if user and data.get("password") == user.password:
+        login_user(user)
+        return jsonify({"message": "Logged in successfully"})
+    return jsonify({"message": "Invalid Login"}), 400
+
+
+@app.route("/api/products/add", methods=["POST"])
 def add_product():
-  data = request.json
-  if 'name' in data and 'price' in data:
-    product = Product(name=data["name"],price=data["price"],description=data.get("description", ""))
-    db.session.add(product)
-    db.session.commit()
-    return jsonify({"message": "Product added successfully"})
-  return jsonify({"message": "Invalid product data"}), 400
+    data = request.json
+    if "name" in data and "price" in data:
+        product = Product(
+            name=data["name"],
+            price=data["price"],
+            description=data.get("description", ""),
+        )
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({"message": "Product added successfully"})
+    return jsonify({"message": "Invalid product data"}), 400
 
-@app.route('/api/products/delete/<int:product_id>', methods=["DELETE"])
+
+@app.route("/api/products/delete/<int:product_id>", methods=["DELETE"])
 def delete_product(product_id):
-  product = Product.query.get(product_id) 
-  if product:
-    db.session.delete(product)
-    db.session.commit()
-    return jsonify({"message": "Product deleted successfully"})
-  return jsonify({"message": "Product not found"}), 404
+    product = Product.query.get(product_id)
+    if product:
+        db.session.delete(product)
+        db.session.commit()
+        return jsonify({"message": "Product deleted successfully"})
+    return jsonify({"message": "Product not found"}), 404
 
-@app.route('/')
+
+@app.route("/api/products/<int:product_id>", methods=["GET"])
+def get_product_details(product_id):
+    product = Product.query.get(product_id)
+    if product:
+        return jsonify(
+            {
+                "id": product.id,
+                "name": product.name,
+                "price": product.price,
+                "description": product.description,
+            }
+        )
+    return jsonify({"message": "Product not found"}), 404
+
+
+@app.route("/api/products/update/<int:product_id>", methods=["PUT"])
+def update_product(product_id):
+    product = Product.query.get(product_id)
+    if not product:
+        return jsonify({"message": "Product not found"}), 404
+
+    data = request.json
+    if "name" in data:
+        product.name = data["name"]
+    if "price" in data:
+        product.price = data["price"]
+    if "description" in data:
+        product.description = data["description"]
+
+    db.session.commit()
+    return jsonify({"message": "Product updated successfully"})
+
+
+@app.route("/api/products", methods=["GET"])
+def get_products():
+    products = Product.query.all()
+    product_list = []
+    for product in products:
+        product_data = {
+            "id": product.id,
+            "name": product.name,
+            "price": product.price,
+        }
+        product_list.append(product_data)
+    return jsonify(product_list)
+
+
+@app.route("/")
 def hello_world():
-  return 'Hello world'
+    return "Hello world"
+
 
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
